389c8ab475b4838ac678bdb49defd610e4d59db8
fanixk <katsimpas.f@gmail.com>
2015-04-13 00:18:56 +0300

    minor change

--- app.js
+++ app.js
@@ -112,5 +112,5 @@ var secureServer = https.createServer(credentials, app).listen(app.get('httpsPor
 
 var server = http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
-});;
+});
 
